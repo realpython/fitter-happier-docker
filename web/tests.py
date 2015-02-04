3e3d8bbe20112edbd61b037ff47fea624fda761f
mjhea0 <hermanmu@gmail.com>
2015-02-04 14:50:49 -0700

    tests

--- web/tests.py
+++ web/tests.py
@@ -4,8 +4,9 @@ import unittest
 class MainTestCase(unittest.TestCase):
 
     def silly_test(self):
-        five = 2 + 2  # two and two always equals five
-        self.assertEqual(five, 5)
+        four = 2 + 2
+        self.assertEqual(four, 4)
+        self.assertNotEqual(four, 5)
 
 
 if __name__ == '__main__':
