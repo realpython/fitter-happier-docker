6c0e7ff22fd5a79cc485dbc4b88ea337aa22c4ab
mjhea0 <hermanmu@gmail.com>
2015-02-06 10:18:06 -0600

    added assert

--- web/tests.py
+++ web/tests.py
@@ -6,6 +6,7 @@ class MainTestCase(unittest.TestCase):
     def test_two_and_two(self):
         four = 2 + 2
         self.assertEqual(four, 4)
+        self.assertNotEqual(four, 5)
 
 
 if __name__ == '__main__':
