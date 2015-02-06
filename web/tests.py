561421d2dd7dd753b1bcb4bba98724572e9e87be
mjhea0 <hermanmu@gmail.com>
2015-02-06 09:47:18 -0600

    pre-fig

--- web/tests.py
+++ web/tests.py
@@ -6,7 +6,6 @@ class MainTestCase(unittest.TestCase):
     def test_two_and_two(self):
         four = 2 + 2
         self.assertEqual(four, 4)
-        self.assertNotEqual(four, 6)
 
 
 if __name__ == '__main__':
