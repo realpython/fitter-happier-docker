e6af25629258f35a05c48df077f0266b02964fdb
Artemis <aleeodm25@gmail.com>
2015-08-29 21:14:47 -0400

    add new test

--- web/tests.py
+++ web/tests.py
@@ -9,6 +9,6 @@ class MainTestCase(unittest.TestCase):
         self.assertNotEqual(four, 5)
         self.assertNotEqual(four, 6)
         self.assertNotEqual(four, 22)
-
+        self.assertNotEqual(four, 25)
 if __name__ == '__main__':
     unittest.main()
