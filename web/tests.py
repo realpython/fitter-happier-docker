27b6870c06bbc21e62a320c9e8bba6f893612551
rnole <rnolec@gmail.com>
2021-10-08 17:28:09 -0500

    my changes

--- web/tests.py
+++ web/tests.py
@@ -8,6 +8,7 @@ class MainTestCase(unittest.TestCase):
         self.assertNotEqual(four, 5)
         self.assertNotEqual(four, 6)
         self.assertNotEqual(four, 22)
+        self.assertNotEqual(four, 102)
 
 
 if __name__ == "__main__":
